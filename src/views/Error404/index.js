deb7463c11096ffba3e3ce906c9fd09a485cb4bd
unknown <rajovicmirko@gmail.com>
2020-10-17 15:54:06 +0200

    Error page small scss fix

--- src/views/Error404/index.js
+++ src/views/Error404/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function index() {
   return (
-    <div className="page page-center">
-      <div className="text-center">
-        <i className="far fa-exclamation-triangle"></i>
-        <div className="display-1 my-4">404</div>
-        <div className="display-4 my-4">Not Found</div>
-        <h6>The resource requested could not be found on this server!</h6>
+    <div className="container">
+      <div className="error-page page page-center mt-5">
+        <div className="text-center">
+          <i className="far fa-exclamation-triangle"></i>
+          <div className="display-1 my-4">404</div>
+          <div className="display-4 my-4">Not Found</div>
+          <h6>The resource requested could not be found on this server!</h6>
+        </div>
       </div>
     </div>
   )
